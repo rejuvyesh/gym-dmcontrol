b19b301dcf5119bcc465b05cfd78e71ec28de75d
rejuvyesh <mail@rejuvyesh.com>
2018-01-23 19:15:40 -0800

    install instructions

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # gym-dmcontrol
 
 Wrapper around [`dm_control`](https://github.com/deepmind/dm_control) to provide a [`gym`](https://github.com/openai/gym/) like interface.
+
+## Installation
+
+```
+$ pip install git+git://github.com/rejuvyesh/gym-dmcontrol.git
+```
--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(
     name='gym_dmcontrol',
-    version='0.0.1',
-    install_requires=['gym', 'dm_control']  # And any other dependencies foo needs
-)
+    version='0.0.2',
+    author='rejuvyesh',
+    description=('Simple wrapper for DeepMind control suite'),
+    license="Apache License, Version 2.0",
+    keywords="openai gym deepmind wrapper",
+    packages=find_packages(),
+    install_requires=['gym', 'dm_control'],)
