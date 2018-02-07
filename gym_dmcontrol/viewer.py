c3c9704640c4ebc6884341de30724b4ebb742bbd
rejuvyesh <mail@rejuvyesh.com>
2018-02-07 14:28:51 -0800

    try again

--- gym_dmcontrol/viewer.py
+++ gym_dmcontrol/viewer.py
@@ -3,18 +3,31 @@ import pyglet
 
 class Viewer:
 
-    def __init__(self, width, height):
+    def __init__(self, width, height, display=None):
         self._width = width
         self._height = height
-        self.window = pyglet.window.Window(width=width, height=height, display=None)
+        self._display = display
+        self.window = None
+        self._isopen = False
 
     def update(self, pixels):
+        if self.window is None:
+            self.window = pyglet.window.Window(width=self._width, height=self._height,
+                                               display=self._display)
+            self._isopen = True
+
+        image = pyglet.image.ImageData(self._width, self._height, 'RGB',
+                                       pixels.tobytes(), pitch=self._width * -3)
         self.window.clear()
         self.window.switch_to()
         self.window.dispatch_events()
-        pyglet.image.ImageData(self._width, self._height, 'RGB',
-                               pixels.tobytes(), pitch=self._width * -3).blit(0, 0)
+        image.blit(0, 0)
         self.window.flip()
 
     def close(self):
-        self.window.close()
+        if self._isopen:
+            self.window.close()
+            self._isopen = False
+
+    def __del__(self):
+        self.close()
